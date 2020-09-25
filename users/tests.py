c2e2e47276b5904f0da8f3519cc6e69ef69197e0
majdalkilany <majdkilany92@gmail.com>
2020-09-25 12:11:58 +0300

    tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from .models import CustomUser
+from django.urls import reverse, reverse_lazy
+class TestLoginView(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='majd',
+            email='majdkilany@gmail.com',
+            password='password'
+        )
+
+        self.user2 = get_user_model().objects.create_user(
+            username='majd',
+            email='majdkilany.com',
+            password='password'
+        )
+
+    def test_the_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code,200)
